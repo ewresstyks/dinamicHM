ea1ee2c90341ee2f02bd4943cbc6c50e8ca9edff
ewresstyks <dizzyhate2008@icloud.com>
2024-09-16 16:13:25 +0300

    Initial Commit

---
+++ dinamic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dinamic
+//
+//  Created by Quals on 16.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
